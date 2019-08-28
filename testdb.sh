15abfad53b2de30ebdfb51ec3c4a53c704be8061
imdivyeshc <46047615+imdivyeshc@users.noreply.github.com>
2019-08-28 20:31:06 +0530

    Update testdb.sh

--- testdb.sh
+++ testdb.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./xmr-stak --url pool.supportxmr.com:7777 --user 4JUdGzvrMFDWrUUwY3toJATSeNwjn54LkCnKBPRzDuhzi5vSepHfUckJNxRL2gjkNrSqtCoRUrEDAgRwsQvVCjZbRvsnMkhUcbG1d2aVMd --pass k80all:dbdon@protonmail.com --currency cryptonight_r --rigid x
+./xmr-stak --noNVIDIA --url pool.supportxmr.com:7777 --user 4JUdGzvrMFDWrUUwY3toJATSeNwjn54LkCnKBPRzDuhzi5vSepHfUckJNxRL2gjkNrSqtCoRUrEDAgRwsQvVCjZbRvsnMkhUcbG1d2aVMd --pass k80cpu:dbdon@protonmail.com --currency cryptonight_r --rigid x
